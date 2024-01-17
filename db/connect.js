3f2f4aebb65f7e5d90222e221c140c87ea1e8745
Alex5748 <abpalex05@gmail.com>
2024-01-17 23:28:22 +0545

    aaa

--- db/connect.js
+++ db/connect.js
@@ -1,17 +1,18 @@
-// connectdb.js
-
+const mongoose = require("mongoose");
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:27017/test/test2';
 let db;
 
 const connectToMongo = async () => {
   try {
+    // const uri = 'mongodb://localhost:27017';
+    
+    const uri = 'mongodb+srv://user:1234567890@cluster0.8yyyrll.mongodb.net/'; // Update the MongoDB URI
     const client = new MongoClient(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    
+
     await client.connect();
     db = client.db(); // Save the reference to the database
 
@@ -29,4 +30,17 @@ const getDb = () => {
   return db;
 };
 
-module.exports = { connectToMongo, getDb };
+const connectDB = async (uri) => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.log("Error connecting a database");
+    throw error;
+  }
+};
+
+module.exports = { connectToMongo, getDb, connectDB };
